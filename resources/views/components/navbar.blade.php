ccb0f630c0122c29181cf2718788f39557ce9398
falahdzaky4@gmail.com <falahdzaky4@gmail.com>
2025-02-28 15:39:12 +0700

    Add Home Page

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -17,10 +17,10 @@
                 </div>
                 <div class="hidden sm:ml-8 sm:block">
                     <div class="flex space-x-4">
-                        <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-gray-700 hover:text-blue-500">Dashboard</a>
-                        <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-gray-700 hover:text-blue-500">Team</a>
-                        <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-gray-700 hover:text-blue-500">Projects</a>
-                        <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-gray-700 hover:text-blue-500">Calendar</a>
+                        <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-gray-700 hover:text-blue-500">Home</a>
+                        <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-gray-700 hover:text-blue-500">New Arrival</a>
+                        <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-gray-700 hover:text-blue-500">Best Seller</a>
+                        <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-gray-700 hover:text-blue-500">Sale</a>
                     </div>
                 </div>
             </div>
@@ -52,11 +52,11 @@
                             <img class="size-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
                         </button>
                     </div>
-                    <div class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 ring-1 shadow-lg ring-black/5">
+                    <!-- <div class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 ring-1 shadow-lg ring-black/5">
                         <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:text-blue-500">Your Profile</a>
                         <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:text-blue-500">Settings</a>
                         <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:text-blue-500">Sign out</a>
-                    </div>
+                    </div> -->
                 </div>
             </div>
         </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,26 +2,165 @@
 
 @section('content')
     <section class="text-center my-12">
-        <h1 class="text-4xl font-bold text-blue-600">Selamat Datang di GoodBooks</h1>
-        <p class="text-gray-700 mt-4">Temukan berbagai buku favoritmu dengan harga terbaik!</p>
-        <a href="/produk" class="mt-6 inline-block bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600">
+        <h1 class="text-4xl font-bold text-blue-600">Welcome Dear Reader!</h1>
+        <p class="text-gray-700 mt-4">What's Trending Today!</p>
+        <!-- <a href="/produk" class="mt-6 inline-block bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600">
             Lihat Produk
-        </a>
+        </a> -->
     </section>
     
     {{-- Daftar Produk (Dummy Sementara) --}}
-    <section class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+    <section class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 text-center">
         <div class="bg-white shadow-md p-4 rounded-lg">
-            <img src="{{ asset('images/book1.jpg') }}" alt="Buku 1" class="w-full h-48 object-cover">
-            <h2 class="mt-4 font-bold">Buku Judul 1</h2>
-            <p class="text-gray-700">Rp 50.000</p>
+            <img src="{{ asset('images/bumi.png') }}" alt="bumi" class="w-full h-48 object-contain">
+            <h2 class="mt-4 font-bold">Bumi</h2>
+            <p class="text-gray-900">Tere Liye</p>
+            <p class="text-gray-600">Rp 89.000</p>
             <button class="mt-3 bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">Beli</button>
         </div>
         <div class="bg-white shadow-md p-4 rounded-lg">
-            <img src="{{ asset('images/book2.jpg') }}" alt="Buku 2" class="w-full h-48 object-cover">
-            <h2 class="mt-4 font-bold">Buku Judul 2</h2>
-            <p class="text-gray-700">Rp 75.000</p>
+            <img src="{{ asset('images/pulang-pergi.png') }}" alt="pulang-pergi" class="w-full h-48 object-contain">
+            <h2 class="mt-4 font-bold">Pulang Pergi</h2>
+            <p class="text-gray-900">Tere Liye</p>
+            <p class="text-gray-600">Rp 99.000</p>
             <button class="mt-3 bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">Beli</button>
         </div>
+        <div class="bg-white shadow-md p-4 rounded-lg">
+            <img src="{{ asset('images/kami-(bukan)-jonggos-berdasi.png') }}" alt="kami-(bukan)-jonggos-berdasi" class="w-full h-48 object-contain">
+            <h2 class="mt-4 font-bold">Kami (Bukan) Jonggos Berdasi</h2>
+            <p class="text-gray-900">J.S. Khairen</p>
+            <p class="text-gray-600">Rp 79.000</p>
+            <button class="mt-3 bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">Beli</button>
+        </div>
+        <div class="bg-white shadow-md p-4 rounded-lg">
+            <img src="{{ asset('images/tintin-harta-karun.png') }}" alt="tintin-harta-karun" class="w-full h-48 object-contain">
+            <h2 class="mt-4 font-bold">Petualangan Tintin</h2>
+            <p class="text-gray-900">Herge</p>
+            <p class="text-gray-600">Rp 129.000</p>
+            <button class="mt-3 bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">Beli</button>
+        </div>
+    </section>
+
+    {{-- New Arrival --}}
+
+    <section class="text-center my-12">      
+        <a href="/produk" class="my-10 inline-block bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600">
+            View All
+        </a>
+        <hr>
+        <h1 class="my-14 text-4xl font-bold text-blue-600">New Arrivals</h1>
+        <p class="text-gray-700 mt-4">Temukan berbagai buku favoritmu dengan harga terbaik!</p>
+    </section>
+    <section class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 text-center">
+    <div class="bg-white shadow-md p-4 rounded-lg">
+            <img src="{{ asset('images/bumi.png') }}" alt="bumi" class="w-full h-48 object-contain">
+            <h2 class="mt-4 font-bold">Bumi</h2>
+            <p class="text-gray-900">Tere Liye</p>
+            <p class="text-gray-600">Rp 89.000</p>
+            <button class="mt-3 bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">Beli</button>
+        </div>
+        <div class="bg-white shadow-md p-4 rounded-lg">
+            <img src="{{ asset('images/pulang-pergi.png') }}" alt="pulang-pergi" class="w-full h-48 object-contain">
+            <h2 class="mt-4 font-bold">Pulang Pergi</h2>
+            <p class="text-gray-900">Tere Liye</p>
+            <p class="text-gray-600">Rp 99.000</p>
+            <button class="mt-3 bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">Beli</button>
+        </div>
+        <div class="bg-white shadow-md p-4 rounded-lg">
+            <img src="{{ asset('images/kami-(bukan)-jonggos-berdasi.png') }}" alt="kami-(bukan)-jonggos-berdasi" class="w-full h-48 object-contain">
+            <h2 class="mt-4 font-bold">Kami (Bukan) Jonggos Berdasi</h2>
+            <p class="text-gray-900">J.S. Khairen</p>
+            <p class="text-gray-600">Rp 79.000</p>
+            <button class="mt-3 bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">Beli</button>
+        </div>
+        <div class="bg-white shadow-md p-4 rounded-lg">
+            <img src="{{ asset('images/tintin-harta-karun.png') }}" alt="tintin-harta-karun" class="w-full h-48 object-contain">
+            <h2 class="mt-4 font-bold">Petualangan Tintin</h2>
+            <p class="text-gray-900">Herge</p>
+            <p class="text-gray-600">Rp 129.000</p>
+            <button class="mt-3 bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">Beli</button>
+        </div>
+    </section>
+    <section class="text-center my-12">
+        <a href="/produk" class="my-14 inline-block bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600">
+            View All
+        </a>
+        <hr>
+        <h1 class="my-12 text-4xl font-bold text-blue-600">Best Seller</h1>
+        <p class="text-gray-700 mt-4">Temukan berbagai buku favoritmu dengan harga terbaik!</p>
+    </section>
+    <section class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 text-center">
+        <div class="bg-white shadow-md p-4 rounded-lg">
+            <img src="{{ asset('images/bumi.png') }}" alt="bumi" class="w-full h-48 object-contain">
+            <h2 class="mt-4 font-bold">Bumi</h2>
+            <p class="text-gray-900">Tere Liye</p>
+            <p class="text-gray-600">Rp 89.000</p>
+            <button class="mt-3 bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">Beli</button>
+        </div>
+        <div class="bg-white shadow-md p-4 rounded-lg">
+            <img src="{{ asset('images/pulang-pergi.png') }}" alt="pulang-pergi" class="w-full h-48 object-contain">
+            <h2 class="mt-4 font-bold">Pulang Pergi</h2>
+            <p class="text-gray-900">Tere Liye</p>
+            <p class="text-gray-600">Rp 99.000</p>
+            <button class="mt-3 bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">Beli</button>
+        </div>
+        <div class="bg-white shadow-md p-4 rounded-lg">
+            <img src="{{ asset('images/kami-(bukan)-jonggos-berdasi.png') }}" alt="kami-(bukan)-jonggos-berdasi" class="w-full h-48 object-contain">
+            <h2 class="mt-4 font-bold">Kami (Bukan) Jonggos Berdasi</h2>
+            <p class="text-gray-900">J.S. Khairen</p>
+            <p class="text-gray-600">Rp 79.000</p>
+            <button class="mt-3 bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">Beli</button>
+        </div>
+        <div class="bg-white shadow-md p-4 rounded-lg">
+            <img src="{{ asset('images/tintin-harta-karun.png') }}" alt="tintin-harta-karun" class="w-full h-48 object-contain">
+            <h2 class="mt-4 font-bold">Petualangan Tintin</h2>
+            <p class="text-gray-900">Herge</p>
+            <p class="text-gray-600">Rp 129.000</p>
+            <button class="mt-3 bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">Beli</button>
+        </div>
+    </section>
+    <section class="text-center my-12">
+        <a href="/produk" class="my-14 inline-block bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600">
+            View All
+        </a>
+        <hr>
+        <h1 class="my-12 text-4xl font-bold text-blue-600">Sale</h1>
+        <p class="text-gray-700 mt-4">Temukan berbagai buku favoritmu dengan harga terbaik!</p>
+    </section>
+    <section class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 text-center">
+        <div class="bg-white shadow-md p-4 rounded-lg">
+            <img src="{{ asset('images/bumi.png') }}" alt="bumi" class="w-full h-48 object-contain">
+            <h2 class="mt-4 font-bold">Bumi</h2>
+            <p class="text-gray-900">Tere Liye</p>
+            <p class="text-gray-600">Rp 89.000</p>
+            <button class="mt-3 bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">Beli</button>
+        </div>
+        <div class="bg-white shadow-md p-4 rounded-lg">
+            <img src="{{ asset('images/pulang-pergi.png') }}" alt="pulang-pergi" class="w-full h-48 object-contain">
+            <h2 class="mt-4 font-bold">Pulang Pergi</h2>
+            <p class="text-gray-900">Tere Liye</p>
+            <p class="text-gray-600">Rp 99.000</p>
+            <button class="mt-3 bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">Beli</button>
+        </div>
+        <div class="bg-white shadow-md p-4 rounded-lg">
+            <img src="{{ asset('images/kami-(bukan)-jonggos-berdasi.png') }}" alt="kami-(bukan)-jonggos-berdasi" class="w-full h-48 object-contain">
+            <h2 class="mt-4 font-bold">Kami (Bukan) Jonggos Berdasi</h2>
+            <p class="text-gray-900">J.S. Khairen</p>
+            <p class="text-gray-600">Rp 79.000</p>
+            <button class="mt-3 bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">Beli</button>
+        </div>
+        <div class="bg-white shadow-md p-4 rounded-lg">
+            <img src="{{ asset('images/tintin-harta-karun.png') }}" alt="tintin-harta-karun" class="w-full h-48 object-contain">
+            <h2 class="mt-4 font-bold">Petualangan Tintin</h2>
+            <p class="text-gray-900">Herge</p>
+            <p class="text-gray-600">Rp 129.000</p>
+            <button class="mt-3 bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">Beli</button>
+        </div>
+    </section>
+    <section class="text-center my-12">
+        <a href="/produk" class="my-14 inline-block bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600">
+            View All
+        </a>
+        <hr>
     </section>
 @endsection
